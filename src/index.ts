ceb7a61510422f31fa9e58c15d777c590a9d55d8
syvixor <syvixor@proton.me>
2026-07-02 18:18:32 +0100

    chore: bump deps & add openapi docs

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 import { Elysia } from "elysia";
 import { node } from "@elysiajs/node";
+import { openapi } from "@elysia/openapi";
 
 new Elysia({ adapter: node() })
-	.get("/", () => {
+    .use(openapi({
+        provider: "scalar",
+        documentation: {
+            info: {
+                title: "API Documentation",
+                version: "0.1.0",
+                description: "OpenAPI Generated Documentation"
+            }
+        }
+    }))
+    .get("/", () => {
         return {
             statusCode: 200,
             statusMessage: "OK"
         }
     })
-	.listen(3000, ({ hostname, port }) => console.log(`Running ➟ ${hostname}:${port}`));
+    .listen(3000, ({ hostname, port }) => console.log(`Running ➟ ${hostname}:${port}`));
